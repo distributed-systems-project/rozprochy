07684b304b33cf9d29b72154bca749f68f3f3652
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2020-01-08 22:11:47 +0100

    replace reservation with submodule

--- reservation-service/src/main/java/pl/edu/agh/distributedsystems/reservationservice/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.distributedsystems.reservationservice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
